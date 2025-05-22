7d41afb5de26caf35c40aeda48159cbeebf77e05
Nolan Donoghue <nolan.donoghue@viewpoint.org>
2025-05-22 15:32:07 -0700

    fix: correct return statement in GuessResults component

--- src/components/GuessResults/GuessResults.jsx
+++ src/components/GuessResults/GuessResults.jsx
@@ -3,12 +3,11 @@ import React from 'react';
 import Guess from '../Guess';
 
 function GuessResults({ guesses, answer }) {
-    <div className="guess-results">
-      {guesses.map((guess, index) => (
-        <Guess key={index} value={guess} answer={answer} />
-      ))}
-    </div>
-  );
+  <div className="guess-results">
+    {guesses.map((guess, index) => (
+      <Guess key={index} value={guess} answer={answer} />
+    ))}
+  </div>;
 }
 
 export default GuessResults;
