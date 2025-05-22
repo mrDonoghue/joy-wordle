b57410ec7bdef88e51e3c6c45f3132bae5719eba
Nolan Donoghue <nolan.donoghue@viewpoint.org>
2025-05-22 10:55:32 -0700

    feat: enhance GuessResults component to display letters of guesses as cells

--- src/components/GuessResults/GuessResults.jsx
+++ src/components/GuessResults/GuessResults.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import { NUM_LETTERS } from '../../constants';
+
 function GuessResults({ guesses }) {
+  const getLetters = (guess) => {
+    return [
+      ...guess.slice(0, NUM_LETTERS).split(''),
+      ...Array(NUM_LETTERS).fill(''),
+    ].slice(0, NUM_LETTERS);
+  };
+
   return (
     <div className="guess-results">
       {guesses.map((guess, index) => (
         <p key={index} className="guess">
-          {guess}
+          {getLetters(guess).map((letter, lindex) => {
+            return (
+              <span key={lindex} className="cell">
+                {letter}
+              </span>
+            );
+          })}
         </p>
       ))}
     </div>
